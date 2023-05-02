570db1c37c0826ea333beb9af8bc16ab6d7d775a
seongyurim <miruyseong@gmail.com>
2023-05-02 22:02:10 +0900

    first commit

--- Hello/src/scannerExam.java
+++ Hello/src/scannerExam.java
@@ -5,18 +5,18 @@ public class scannerExam {
         // 데이터를 사용자로부터 입력받는다.
 
 
-        // 키보드를 스캔하는 객체를 만들어준다.
+        // a. 키보드를 스캔하는 객체를 만들어준다.
         Scanner scan = new Scanner(System.in); // 사용자에게 입력받는 코드(우선 이대로 이해하자)
                                                // 스캐너라는 객체를 Heap에 만들어서 참조값을 가져오는 것
                                                // System.in = 시스템의 input device이므로 키보드라고 생각하자
 
-        // 키보드에서 엔터키입력까지 읽어온다.
+        // b. 키보드에서 엔터키입력까지 읽어온다.
         String inputStr = scan.nextLine();
 
-        // 읽어온 문자열을 int로 변환한다.
+        // c. 읽어온 문자열을 int로 변환한다.
         int val = Integer.parseInt(inputStr);
 
-        // 100을 더한 후 찍어준다.
+        // d. 100을 더한 후 출력해본다.
         val = val + 100;
         System.out.println(val);
     }
