a8c8d83975bd1b704957a66564f180b0cd4aa457
seongyurim <miruyseong@gmail.com>
2023-05-02 14:02:25 +0900

    first commit

--- Hello/src/App.java
+++ Hello/src/App.java
@@ -1,5 +1,24 @@
 public class App {
     public static void main(String[] args) throws Exception {
+
+        // 미션1 : 두 수를 더하는 코드를 구현해 봅시다.
+
+        // 정수
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
+
+        // (배정도) 실수
+        double d = 10.5;
+        double f = 2.3;
+        System.out.println(d + f);
+
+        // (단정도) 실수
+        float r = 10.2f;
+        float k = 4.0f;
+        System.out.println(r + k);
+
+
         System.out.println("Hello, World!");
     }
 }
