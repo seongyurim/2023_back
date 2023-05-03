3560d175356f42f863324cadced0ea200b41c76a
seongyurim <miruyseong@gmail.com>
2023-05-03 09:19:31 +0900

    first commit

--- Hello/src/typeCasting.java
+++ Hello/src/typeCasting.java
@@ -19,12 +19,12 @@ public class typeCasting {
         // float f1 = 3.14f;
         // int ii = f1;
         // 사이즈'만' 맞을 뿐, 비트스트림이 서로 완전히 다르다. 비상식적인 변환이다.
-        // int ii = (int)f1;로 억지로 끼워넣을 수야 있겠지만 소수점을 제거하고 3만 반환한다.
+        // int ii = (int)f1;로 억지로 끼워넣을 수야 있겠지만 소수점을 제거하고 3만 반환된다.
 
         int fff = 4;
         float ef = fff;
         System.out.println(ef);
-        // 묵시적 형변환이 가능한 경우
+        // 묵시적 형변환이 가능한 경우이다.
         
     }
 }
