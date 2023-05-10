a63f93ab6fcc1b2213f43ac353ed697b947dc6d8
seongyurim <miruyseong@gmail.com>
2023-05-10 17:31:00 +0900

    first commit

--- Book251/src/App.java
+++ Book251/src/App.java
@@ -3,25 +3,25 @@ import transport.Car;
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Car car1 = new Car();
-        car1.setCompany("Hyundai");
-        car1.setModel("Sonata");
-        car1.setColor("Yellow");
-        car1.setMaxSpeed(200);
+        Car avante = new Car();
+        avante.setCompany("Hyundai");
+        avante.setModel("Normal");
+        avante.setColor("Yellow");
+        avante.setMaxSpeed(200);
 
-        System.out.printf("car1의 제조사: %s\n", car1.company);
-        System.out.printf("car1의 모델: %s\n", car1.model);
-        System.out.printf("car1의 색상: %s\n", car1.color);
-        System.out.printf("car1의 최대속도: %s\n", car1.maxSpeed);
+        System.out.printf("car1의 제조사: %s\n", avante.getCompany());
+        System.out.printf("car1의 모델: %s\n", avante.getModel());
+        System.out.printf("car1의 색상: %s\n", avante.getColor());
+        System.out.printf("car1의 최대속도: %s\n", avante.getMaxSpeed());
 
         /////////////////////////////////////////////////////////////////////////
 
-        Car car2 = new Car("KIA", "Carnival", "Black", 250);
+        Car sonata = new Car("Hyundai", "N-Line", "Black", 250);
 
-        System.out.printf("car2의 제조사: %s\n", car2.getCompany());
-        System.out.printf("car2의 모델: %s\n", car2.getModel());
-        System.out.printf("car2의 색상: %s\n", car2.getColor());
-        System.out.printf("car2의 최대속도: %s\n", car2.getMaxSpeed());
+        System.out.printf("car2의 제조사: %s\n", sonata.getCompany());
+        System.out.printf("car2의 모델: %s\n", sonata.getModel());
+        System.out.printf("car2의 색상: %s\n", sonata.getColor());
+        System.out.printf("car2의 최대속도: %s\n", sonata.getMaxSpeed());
 
     }
 }
--- Book251/src/transport/Car.java
+++ Book251/src/transport/Car.java
@@ -1,41 +1,46 @@
 package transport;
 public class Car {
     
-    // FIELD
-    public String company;
-    public String model;
-    public String color;
-    public int maxSpeed;
+    // FIELD -----------------------------------------------------------------------------------
+    private String company;
+    private String model;
+    private String color;
+    private int maxSpeed;
 
-    // DEFAULT CONSTRUCTOR
+
+    // DEFAULT CONSTRUCTOR ---------------------------------------------------------------------
     public Car()
     {
-        System.out.println("*** DEFAULT CONSTRUCTOR ***");
+        System.out.println("***** DEFAULT CONSTRUCTOR *****");
         company = "";
         model = "";
         color = "";
         maxSpeed = 0;
     }
 
-    // OVERLOADED CONSTRUCTOR
+
+    // OVERLOADED CONSTRUCTOR ------------------------------------------------------------------
     public Car(String company, String model, String color, int maxSpeed)
     {
-        System.out.println("*** OVERLOADED CONSTRUCTOR ***");
+        System.out.println("***** OVERLOADED CONSTRUCTOR *****");
         this.company = company;
         this.model = model;
         this.color = color;
         this.maxSpeed = maxSpeed;
     }
 
-    // SETTER
+
+    // 필드값을 private로 선언했기 때문에 getter/setter 필요
+    // SETTER -----------------------------------------------------------------------------------
     public void setCompany(String company) { this.company = company; };
     public void setModel(String model)     { this.model = model; };
     public void setColor(String color)     { this.color = color; };
     public void setMaxSpeed(int maxSpeed)  { this.maxSpeed = maxSpeed; };
 
-    // GETTER
-    public String getCompany()  { return company; };
-    public String getModel()    { return model; };
-    public String getColor()    { return color; };
-    public int getMaxSpeed()    { return maxSpeed; };
+
+    // GETTER -----------------------------------------------------------------------------------
+    public String getCompany()  { return this.company; };
+    public String getModel()    { return this.model; };
+    public String getColor()    { return this.color; };
+    public int getMaxSpeed()    { return this.maxSpeed; };
 }
