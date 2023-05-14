06354a760ce40f8e444fc26394091e112986e184
seongyurim <miruyseong@gmail.com>
2023-05-15 00:55:48 +0900

    first commit

--- J007_Singleton/src/Account1920/Account.java
+++ J007_Singleton/src/Account1920/Account.java
@@ -5,35 +5,40 @@ public class Account {
     // Attribute
     public static final int MIN_BALANCE = 0;
     public static final int MAX_BALANCE = 1000000;
-    private int balance;
     private String accountNum;
-    private String name;
-    private int payment;
+    private String owner;
+    private int balance;
 
 
     // Default Constructor
     public Account() {
         accountNum = "";
-        name = "";
-        payment = 0;
+        owner = "";
         balance = 0;
+    }
+
+    // Overloaded Constructor
+    public Account(String accountNum, String owner, int balance) {
+        this.accountNum = accountNum;
+        this.owner = owner;
+        this.balance = balance;
     }
     
 
     // Getter
     public String getAccountNum() { return accountNum; };
-    public String getName()       { return name; };
-    public int getPayment()       { return payment; };
+    public String getName()       { return owner; };
     public int getBalance()       { return balance; };
 
-    // Setter
-    public boolean setBalance(int balance) {
 
-        if ((balance < Account.MIN_BALANCE) ||
-            (balance > Account.MAX_BALANCE)) {
-            return false;
+    // Setter
+    public void setAccountNum(String accountNum) { this.accountNum = accountNum; };
+    public void setOwner(String owner) { this.owner = owner; };
+    public void setBalance(int balance) {
+        if ((balance > Account.MIN_BALANCE) &&
+            (balance < Account.MAX_BALANCE)) {
+            this.balance = balance;
         }
-        this.balance = balance;
-        return true;
     }
+
 }
--- J007_Singleton/src/Account1920/BankApplication.java
+++ J007_Singleton/src/Account1920/BankApplication.java
@@ -3,24 +3,119 @@ package Account1920;
 import java.util.Scanner;
 
 public class BankApplication {
+    public static void main(String[] args) {
+
+        Account newAcc = new Account();
+
+        
+        // 스캐너를 위한 속성
+        Scanner scan = new Scanner(System.in);
+        String userInput = null;
+
+        // 엔티티 도출
+        Account[] accountArray = new Account[100];
+        int menu = 0;
     
-    // Attribute
-    Scanner scan = new Scanner(System.in);
-    String userInput = null;
-    int[] Account = null;
-    int menu = 0;
+        do {
+    
+            System.out.println("-----------------------------------------------------------");
+            System.out.println("| 1. 계좌생성 | 2. 계좌목록 | 3. 예금 | 4. 출금 | 5. 종료 |");
+            System.out.println("-----------------------------------------------------------");
+            System.out.printf("선택> ");
+            userInput = scan.nextLine();
 
-    do {
+            menu = Integer.parseInt(userInput);
+    
+            switch(menu) {
 
-        System.out.println("-----------------------------------------------------------");
-        System.out.println("| 1. 계좌생성 | 2. 계좌목록 | 3. 예금 | 4. 출금 | 5. 종료 |");
-        System.out.println("-----------------------------------------------------------");
-        System.out.printf("선택> ");
-        userInput = scan.nextLine();    
-        menu = Integer.parseInt(userInput);
+                case 1: // 계좌생성
+                    System.out.println("---------------");
+                    System.out.println("| 1. 계좌생성 |");
+                    System.out.println("---------------");
 
-    } while (menu != 5);
-    System.out.println("프로그램을 종료합니다.");
+                    System.out.printf("계좌번호: ");
+                    String accountNum =  scan.nextLine();
+
+                    System.out.printf("계좌주: ");
+                    String accountOwner =  scan.nextLine();
+
+                    System.out.printf("초기입금액: ");
+                    int accountBal =  Integer.parseInt(scan.nextLine());
+
+                    Account newAccount = new Account(accountNum, accountOwner, accountBal);
+                    System.out.println("결과: 계좌가 생성되었습니다.");
+
+                    for (int i = 0; i < accountArray.length; i++) {
+                        if (accountArray[i] == null) {
+                            accountArray[i] = newAccount;
+                            break;
+                        }
+                    }
+                break;
+
+                case 2: // 계좌목록
+                    System.out.println("---------------");
+                    System.out.println("| 2. 계좌목록 |");
+                    System.out.println("---------------");
+
+                    for (int i = 0; i < accountArray.length; i++) {
+                        if (accountArray[i] != null) {
+                            System.out.printf(accountArray[i].getAccountNum() + "\t" +
+                                              accountArray[i].getName() + "\t" +
+                                              accountArray[i].getBalance() + "\n");
+                        }
+                    }
+                break;
+
+                case 3: // 예금
+                System.out.println("---------------");
+                System.out.println("| 3. 예금하기 |");
+                System.out.println("---------------");
+
+                System.out.printf("계좌번호: ");
+                accountNum =  scan.nextLine();
+
+                System.out.printf("예금액: ");
+                int deposit =  Integer.parseInt(scan.nextLine());
+
+                for(int i = 0; i < accountArray.length; i++) {
+                    if (accountArray[i] != null) {
+                        if (accountArray[i].getAccountNum().equals(accountNum)) {
+                            accountArray[i].setBalance(accountArray[i].getBalance() + deposit);
+                        }
+                    }
+                }
+                break;
+
+                case 4: // 출금
+                System.out.println("---------------");
+                System.out.println("| 4. 출금하기 |");
+                System.out.println("---------------");
+
+                System.out.printf("계좌번호: ");
+                accountNum =  scan.nextLine();
+
+                System.out.printf("출금액: ");
+                int withdraw =  Integer.parseInt(scan.nextLine());
+
+                for(int i = 0; i < accountArray.length; i++) {
+                    if (accountArray[i] != null) {
+                        if (accountArray[i].getAccountNum().equals(accountNum)) {
+                            accountArray[i].setBalance(accountArray[i].getBalance() - withdraw);
+                        }
+                    }
+                }
+                break;
+
+                default:
+                System.out.println("[ERROR] BankApplication Class");
+                break;
+            }
+    
+        } while(menu != 5);
+        System.out.println("프로그램을 종료합니다.");
+    }
+    
 
 
 }
